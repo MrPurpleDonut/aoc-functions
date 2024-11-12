98febe29393d6ac6f1ac5b9c6b94fb792fc048f0
Aidan Thomas <aidanthomas@Aidans-MacBook-Air.local>
2024-11-11 22:06:18 -0600

    buidling make matrix

---
+++ functions.go
@@ -0,0 +1,20 @@
+package aocfunctions
+
+import (
+	"os"
+	"strings"
+)
+
+func MakeMatrix(filename string) ([][]rune, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	vals := strings.Split(string(data), "\n")
+
+	rows := make([][]rune, len(vals))
+	for i, row := range vals {
+		rows[i] = []rune(row)
+	}
+	return rows, nil
+}
