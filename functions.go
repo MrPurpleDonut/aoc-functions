5a575d56315e6832f7c823cc40dfca4a0d1cc2ea
Aidan Thomas <aidanthomas@Aidans-MacBook-Air.local>
2024-12-10 22:02:07 -0600

    adding more matrix stuff

--- functions.go
+++ functions.go
@@ -2,10 +2,11 @@ package aoc_functions
 
 import (
 	"os"
+	"strconv"
 	"strings"
 )
 
-func PanicOnErr(err error) {
+func HandleError(err error) {
 	if err != nil {
 		panic(err)
 	}
@@ -23,6 +24,35 @@ func MakeMatrix(filename string) ([][]rune, error) {
 		rows[i] = []rune(row)
 	}
 	return rows, nil
+}
+
+func MakeIntMatrix(filename string) ([][]int, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	vals := strings.Split(string(data), "\n")
+
+	rows := make([][]int, len(vals))
+	for i, row := range vals {
+		rows[i] = make([]int, len(row))
+		for j, char := range row {
+			val, err := strconv.Atoi(string(char))
+			if err != nil {
+				return nil, err
+			}
+			rows[i][j] = val
+		}
+	}
+	return rows, nil
+}
+
+func MakeBoolBoard(rows, cols int) [][]bool {
+	board := make([][]bool, rows)
+	for i := range board {
+		board[i] = make([]bool, cols)
+	}
+	return board
 }
 
 func MakeRows(filename string) ([]string, error) {
